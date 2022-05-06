24cddf2f34bc1d4018a1e122de241ff47291865f
legendjagdish1234 <legendjagdish1234@gmail.com>
2022-05-06 07:49:34 +0545

    commit

---
+++ FileUpload/Database/DBConnection.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using FileUpload.Model;
+
+namespace FileUpload.Database
+{
+    public class DBConnection : DbContext
+    {
+        public DBConnection()
+        {
+        }
+
+        public DBConnection(DbContextOptions<DBConnection> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<UploadFile> UploadFile { get; set; }
+        public virtual DbSet<AttachementDetails> AttachementDetails { get; set; }
+    }
+}
